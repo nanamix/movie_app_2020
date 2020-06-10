e570255e2d7fb364784ed4a1bd5936433ca8bd96
unknown <jyha81@gmail.com>
2020-06-10 16:41:11 +0900

    03-3-03 JSX 반환

--- src/Potato.js
+++ src/Potato.js
@@ -1,1 +1,7 @@
 import React from 'react';  // 이코드를 입력해야 리액트가 JSX를 이해할수 있음. 
+
+//컴포넌트작성 시 이름은 대문자로 시작해야 함. 
+//컴포넌트가 반환할 값은 JSX로 작성
+ function Potato() {
+     return <h3>I love Potato</h3>; // ← HTML이 아닌 JSX
+ }
