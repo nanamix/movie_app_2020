9f09c4bae166d3764c97fa4c1597d0c90c6b1c49
unknown <jyha81@gmail.com>
2020-06-10 16:46:32 +0900

    03-3-08

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Potato from './Potato';
 //App 컴포넌트 정의
 function App() {
   //return <div className="App"/>;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App';  //App 컴포넌트를 임포트하여 사용
 
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+    //App 컴포넌트를 임포트하여 사용
+    <App />,document.getElementById('root')
 );
 
