07635c95b667010fd3144c283dd432a275b1e224
unknown <jyha81@gmail.com>
2020-06-10 16:47:05 +0900

    03-3-09

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Potato from './Potato';
+
 //App 컴포넌트 정의
 function App() {
   //return <div className="App"/>;
@@ -7,7 +8,8 @@ function App() {
   //App 컴포넌트는 HTML을 반환하고 있음.
   return (
     <div>
-    <h1>Hello!!!</h1>
+      <h1>Hello!!!</h1>
+      <Potato />
     </div>
   );
 }
