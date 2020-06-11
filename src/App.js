2a20429c57c583f83b25e35efd8f9332d6934d80
nanamix <nanamix@naver.com>
2020-06-12 00:12:58 +0900

    05-4-Final

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ class App extends React.Component {
   //컴포넌트가 마운트되면 true(처음에는 로딩상태)
   state = {
     isLoading: true,
+    movies : [],  //로딩된 영화데이터를 저장할 수 있도록 movies state를 만듦.
   };
 
   componentDidMount(){
+    //영화 데이터 로딩!
     setTimeout(() =>{
       this.setState({ isLoading : false });
     }, 6000);
