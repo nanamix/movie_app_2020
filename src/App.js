8a6a9205022cf0c971971a8871e0e40eadde7330
nanamix <nanamix@naver.com>
2020-06-12 00:38:16 +0900

    06-14

--- src/App.js
+++ src/App.js
@@ -9,10 +9,16 @@ class App extends React.Component {
     movies : [],  //로딩된 영화데이터를 저장할 수 있도록 movies state를 만듦.
   };
 
+  getMovies = async () =>{ //async => 자바스크립트에게 getMovies()함수는 시간이 필요하고
+    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json'); //await => axios.get()의 실행을 기다려 달라고 말해줌
+  };
+
   componentDidMount(){
     //영화 데이터 로딩!
-    axios.get('https://yts.mx/api/v2/list_movies.json');
-  }
+    this.getMovies();
+  };
+
+
   render(){
         const { isLoading } = this.state;
         return <div>{isLoading ? 'Loading ... ' : 'we are ready'}</div>;
