b0dc13fed66818200b90fc8f6f71e2957042ede0
nanamix <nanamix@naver.com>
2020-06-12 00:08:22 +0900

    05-4-03

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 class App extends React.Component {
 
-  render(){
-        return <div/>
-      }
-    );
+  //컴포넌트가 마운트되면 true(처음에는 로딩상태)
+  state = {
+    isLoading: true,
+  };
 
-  }
+  render(){
+        const { isLoading } = this.state;
+        return <div>{isLoading ? 'Loading ... ' : 'we are ready'}</div>;
+      }
+
+
 }
 
 export default App;
