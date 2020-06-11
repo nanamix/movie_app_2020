0691aebd2dc5df8c3a77b1afeecbf396cf426167
nanamix <nanamix@naver.com>
2020-06-12 00:32:42 +0900

    06-13

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
 
@@ -10,9 +11,7 @@ class App extends React.Component {
 
   componentDidMount(){
     //영화 데이터 로딩!
-    setTimeout(() =>{
-      this.setState({ isLoading : false });
-    }, 6000);
+    axios.get('https://yts.mx/api/v2/list_movies.json');
   }
   render(){
         const { isLoading } = this.state;
