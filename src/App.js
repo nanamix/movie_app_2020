1be12bc90a5798f954b2c5fde3ce5b6b1bc902a5
unknown <jyha81@gmail.com>
2020-06-11 17:09:26 +0900

    05-2

--- src/App.js
+++ src/App.js
@@ -11,12 +11,24 @@ class App extends React.Component {
   add = () =>{
     //this.state.count = 1;
     //this.setState( { count : 1 });
-    this.setState( { count : this.state.count + 1 })
+    //this.setState( { count : this.state.count + 1 })
+    this.setState(current => (
+        {
+          count : current.count + 1
+        }
+      )
+    );
   };
 
   minus = () =>{
     //this.setState( { count : -1 });
-    this.setState( { count : this.state.count - 1 })
+    //this.setState( { count : this.state.count - 1 })
+    this.setState(current => (
+        {
+          count : current.count -1
+        }
+      )
+    );
   };
 
   render(){
