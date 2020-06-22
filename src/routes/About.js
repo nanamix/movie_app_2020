d4398a6646df0e3c257ce9f0675492dec3baebc9
nanamix <jyha81@gmail.com>
2020-06-23 00:00:48 +0900

    08-2

---
+++ src/routes/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+    // return <span>About this page: I built it because I love music</span>
+    return (
+        <div className="about__container">
+            <span>
+                "Freedom is thre freedom to say that two plus two make four. If that is granted, all else follows"
+            </span>
+            <span>
+                - George Orwell, 1984
+            </span>
+        </div>
+    );
+}
+
+export default About;
