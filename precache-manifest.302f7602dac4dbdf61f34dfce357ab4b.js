8419999cf8fe54abbce67a541cd47fe3d44bed9c
nanamix <jyha81@gmail.com>
2020-06-23 23:15:59 +0900

    Updates

---
+++ precache-manifest.302f7602dac4dbdf61f34dfce357ab4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d408ee55bb0c822684befbc421ab7ab9",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "700e276fb0a8ad7e10f8",
+    "url": "/movie_app_2020/static/css/main.257ff78e.chunk.css"
+  },
+  {
+    "revision": "7af196d69da60ae2004b",
+    "url": "/movie_app_2020/static/js/2.366baa00.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.366baa00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "700e276fb0a8ad7e10f8",
+    "url": "/movie_app_2020/static/js/main.0fb9262c.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
